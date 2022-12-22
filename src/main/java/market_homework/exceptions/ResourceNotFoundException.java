79f61b899934f57bdeaf46cee961767cf38839fa
alexey_pereverzev <aper-hari@list.ru>
2022-12-22 13:00:39 +0600

    Lesson05 - Spring Cloud part 1

--- src/main/java/market_homework/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package market_homework.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
